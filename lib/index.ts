f4e3c4c211460c5c17b22b0f7376a3e9b2fcbd6a
yuwang <yuwang@wiscom.com.cn>
2024-11-07 16:21:45 +0800

    新增折现图表组件

--- lib/index.ts
+++ lib/index.ts
@@ -1,9 +1,11 @@
 import BarChart from "./barChart/barChart";
 import ChordDiagram from "./chordDiagram/chordDiagram";
 import ImageView from "./imageView/imageView";
+import LineChart from "./lineChart/lineChart";
 const WisChart = {
   BarChart,
   ChordDiagram,
   ImageView,
+  LineChart,
 };
 export default WisChart;
